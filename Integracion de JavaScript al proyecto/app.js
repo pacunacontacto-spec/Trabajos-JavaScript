9f5051fb0f20befc505dbd461cce991fce7268d0
pacunacontacto-spec <p.acuna.contacto@gmail.com>
2026-03-18 22:27:55 -0300

    Add files via upload

---
+++ Integracion de JavaScript al proyecto/app.js
@@ -0,0 +1,32 @@
+console.log("javascript conectado correctamente")
+
+const sum1 = 5;
+const sum2 = 6;
+
+console.log(sum1 * sum2)
+
+const sum3 = 5;
+const sum4 = 6;
+const suma = sum3 + sum4;
+
+console.log(suma)
+
+console.log("JAVASCRIPT IS COMING")
+
+document.getElementById("boton").onclick = function() {
+
+ window.scrollTo ( {
+    top: 0,
+    behavior: "smooth"
+ });
+
+console.log("Hola!")
+
+}
+
+
+
+
+
+
+
